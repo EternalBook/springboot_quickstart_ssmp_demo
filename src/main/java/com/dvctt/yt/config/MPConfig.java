fdb8f2ab45938497ec553d9605d91771af4fa070
EternalBook <2435320866@qq.com>
2023-04-19 10:57:43 +0800

    Create README.md

--- src/main/java/com/dvctt/yt/config/MPConfig.java
+++ src/main/java/com/dvctt/yt/config/MPConfig.java
@@ -5,6 +5,9 @@ import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerIntercept
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * @author etern
+ */
 @Configuration
 public class MPConfig {
     @Bean
